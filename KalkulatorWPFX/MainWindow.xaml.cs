5e16d7bcafe42b16bb007fcac643e0215949b238
kamilxkruk@gmail.com <kamilxkruk@gmail.com>
2019-11-28 17:31:41 +0100

    Initial commit

---
+++ KalkulatorWPFX/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using KalkulatorWPFX.View;
+
+namespace KalkulatorWPFX
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserControl calculatorView;
+        private UserControl homeView;
+        private UserControl palindromView;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculatorView = new CalculatorView();
+            homeView = new HomeView();
+        }
+
+        private void Home_OnClick(object sender, RoutedEventArgs e)
+        {
+            ContentPresenter.Content = homeView;
+        }
+
+        private void Calculator_OnClick(object sender, RoutedEventArgs e)
+        {
+            ContentPresenter.Content = calculatorView;
+        }
+
+    }
+}
---
+++ KalkulatorWPFX/View/CalculatorView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KalkulatorWPFX.View
+{
+    /// <summary>
+    /// Interaction logic for CalculatorView.xaml
+    /// </summary>
+    public partial class CalculatorView : UserControl
+    {
+        public CalculatorView()
+        {
+            InitializeComponent();
+        }
+
+        private void Plus_OnClick(object sender, RoutedEventArgs e)
+        {
+            var x = Convert.ToInt32(XTextBox.Text);
+            var y = Convert.ToInt32(YTextBox.Text);
+
+            ResultTextBlock.Text = (x + y).ToString();
+        }
+
+        private void Minus_OnClick(object sender, RoutedEventArgs e)
+        {
+            var x = Convert.ToInt32(XTextBox.Text);
+            var y = Convert.ToInt32(YTextBox.Text);
+
+            ResultTextBlock.Text = (x - y).ToString();
+        }
+    }
+}
